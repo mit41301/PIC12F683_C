f46e775994ccfb0edbc238613cdc9830e79ed2bf
mit41301 <120297041+mit41301@users.noreply.github.com>
2026-06-02 20:10:04 +0530

    Delete ir-remocon/ir-remocon directory

--- ir-remocon/ir-remocon/scif_tx.h
+++
@@ -1,8 +0,0 @@
-void scif_tx_init(void);
-void putch(unsigned char);
-void puts(const unsigned char *);
-void puthc(unsigned char);
-void putui(unsigned int, unsigned char);
-
-void deb_sig(const unsigned char *, unsigned int);
-void deb_dat(unsigned int, unsigned char);
